63e0da10dd9ff78aadfcc00610203f1b75dd84cb
Vinicius Nardelli <vinicius.goulart1999@gmail.com>
2026-08-14 12:57:24 +0000

    renamed columns aliases in staging models

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -9,11 +9,11 @@ source as (
 renamed as (
 
     select
-        id,
-        user_id,
-        order_date,
-        status,
-        _etl_loaded_at
+       id as order_id,
+       user_id as customer_id,
+       order_date,
+       status
+       _etl_loaded_at
 
     from source
 
--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -9,12 +9,12 @@ source as (
 renamed as (
 
     select
-        id,
-        orderid,
-        paymentmethod,
-        status,
-        amount,
-        created,
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        amount as payment_amount,
+        created as payment_created,
         _batched_at
 
     from source
